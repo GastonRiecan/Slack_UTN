4210e25d0ee074ad12cb4f304849442700e7edd4
Gaston_Riecan <g.e.riecan@gmail.com>
2024-07-30 22:57:11 -0300

    Mock and images added

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+
+export const users = [
+    {
+        id: 1,
+        userName: "Pepe",
+        profilePicture: "Pepe.jpg"
+    }
+]
+
+
+
+export const workSpaces = [
+    {
+        id: 1,
+        name: "General",
+        thumbnail: "defaultWorkSpaceImage.png",
+        channels: [
+            {
+                id: 1,
+                name: "General",
+                messages: [
+                    {
+                        id: 1,
+                        userID: 1,
+                        timeStamp: "17/04/2024, 12:05",
+                        content: "Este es el contenido del mensaje"
+                    }
+                ]
+            }
+        ]
+    },
+
+]
