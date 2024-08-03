b4ddf546bc8b2efc095d18aa9e1235e97cb541ae
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-03 16:14:09 -0300

    Mock data updated

--- src/data/data.js
+++ src/data/data.js
@@ -22,6 +22,55 @@ export const workSpaces = [
                     {
                         id: 1,
                         userID: 1,
+                        timeStamp: "2/04/2024, 12:05",
+                        content: "mensajes 1"
+                    },
+                    {
+                        id: 2,
+                        userID: 1,
+                        timeStamp: "13/04/2024, 12:05",
+                        content: "mensajes 2"
+                    },
+                    {
+                        id: 3,
+                        userID: 1,
+                        timeStamp: "17/04/2022, 12:05",
+                        content: "mensajes 3"
+                    },
+                    {
+                        id: 4,
+                        userID: 1,
+                        timeStamp: "6/04/2024, 12:05",
+                        content: "mensajes 4"
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name: "Preguntas",
+                messages: [
+                    {
+                        id: 1,
+                        userID: 1,
+                        timeStamp: "17/04/2024, 12:05",
+                        content: "Mensaje del canal preguntas"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "General",
+        thumbnail: "defaultWorkSpaceImage.png",
+        channels: [
+            {
+                id: 1,
+                name: "General2",
+                messages: [
+                    {
+                        id: 1,
+                        userID: 1,
                         timeStamp: "17/04/2024, 12:05",
                         content: "Este es el contenido del mensaje"
                     }
