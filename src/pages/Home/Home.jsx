2d4d340404969496e858a9138811387a1ab5aea9
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-03 16:10:24 -0300

    Dynamic workspaces rendering

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { workSpaces } from "../../data/data.js";
 
 const Home = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
       <h1>Bienvenido a Slack</h1>
       <h2>Entornos de trabajo:</h2>
-      <section
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          border: "1px solid black",
-          borderRadius: "10px",
-          padding: "2px",
-        }}
-      >
-        <img src="/images/workspaces/defaultWorkSpaceImage.png" width="50px" />
-        <span>Workspace_name</span>
-        <Link to="/workspace/1/1">
-          <h2>Entrar</h2>
-        </Link>
-      </section>
+      {workSpaces.map((workSpace) => (
+        <section
+          key={workSpace.id}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            border: "1px solid black",
+            borderRadius: "10px",
+            padding: "2px",
+          }}
+        >
+          <img src={`images/workspaces/${workSpace.thumbnail}`} width="50px" />
+          <span>{workSpace.name}</span>
+          <Link to={`/workspace/${workSpace.id}/${workSpace.channels[0].id}`}>
+            <h2>Entrar</h2>
+          </Link>
+        </section>
+      ))}
       <section
         style={{
           display: "flex",
