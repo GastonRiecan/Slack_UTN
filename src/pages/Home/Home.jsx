a11c35fb5a57eebe0c130939bb96b3aaf611d7b4
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-05 16:18:27 -0300

    home styles added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,15 +8,17 @@ const Home = () => {
     <div className="home-container">
       <h1>Bienvenido a Slack</h1>
       <h2>Entornos de trabajo:</h2>
-      {workSpaces.map((workSpace) => (
-        <section className="workspaces" key={workSpace.id}>
-          <img src={`images/workspaces/${workSpace.thumbnail}`}/>
-          <span>{workSpace.name}</span>
-          <Link to={`/workspace/${workSpace.id}/${workSpace.channels[0].id}`}>
-            <h2>Entrar</h2>
-          </Link>
-        </section>
-      ))}
+      <div className="section-wrapper">
+        {workSpaces.map((workSpace) => (
+          <section className="workspaces" key={workSpace.id}>
+            <img src={`images/workspaces/${workSpace.thumbnail}`} />
+            <span>{workSpace.name}</span>
+            <Link to={`/workspace/${workSpace.id}/${workSpace.channels[0].id}`}>
+              <h2>Entrar</h2>
+            </Link>
+          </section>
+        ))}
+      </div>
       <section className="new-workspace">
         <Link to="/workspace/new">
           <h2>Crear Entorno</h2>
