558d415f0bfa36b4eb91aa6643f619f422e21aa8
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-12 20:01:47 -0300

    Workspaces component created

---
+++ src/components/Workspaces/Workspaces.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useWorkspacesContext } from "../../contexts/WorkspacesContext";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+const Workspaces = () => {
+  const { workSpaces } = useWorkspacesContext();
+
+  return (
+    <>
+      <div className="section-wrapper">
+        {workSpaces.map((workSpace) => (
+          <section className="workspaces" key={workSpace.id}>
+            <img src={`images/workspaces/${workSpace.thumbnail}`} />
+            <span>{workSpace.name}</span>
+            <Link to={`/workspace/${workSpace.id}/${workSpace.channels[0].id}`}>
+              <h2>Entrar</h2>
+            </Link>
+          </section>
+        ))}
+      </div>
+      <section className="new-workspace">
+        <Link to="/workspace/new">
+          <h2>Crear Entorno</h2>
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default Workspaces;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import Workspaces from "../../components/Workspaces/Workspaces.jsx";
 import "./styles.css";
-import { useWorkspacesContext } from "../../contexts/WorkspacesContext.jsx";
 
 const HomePage = () => {
-  const { workSpaces } = useWorkspacesContext();
-
   return (
     <div className="home-container">
       <h1>Bienvenido a Slack</h1>
       <h2>Entornos de trabajo:</h2>
-      <div className="section-wrapper">
-        {workSpaces.map((workSpace) => (
-          <section className="workspaces" key={workSpace.id}>
-            <img src={`images/workspaces/${workSpace.thumbnail}`} />
-            <span>{workSpace.name}</span>
-            <Link to={`/workspace/${workSpace.id}/${workSpace.channels[0].id}`}>
-              <h2>Entrar</h2>
-            </Link>
-          </section>
-        ))}
-      </div>
-      <section className="new-workspace">
-        <Link to="/workspace/new">
-          <h2>Crear Entorno</h2>
-        </Link>
-      </section>
+      <Workspaces />
     </div>
   );
 };
