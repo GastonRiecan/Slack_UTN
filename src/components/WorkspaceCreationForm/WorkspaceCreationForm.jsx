97bf0f2a61f92d0b3897c9a9fcbe38d6dae2c8b9
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-12 19:37:07 -0300

    WorkspaceCreationForm component created

---
+++ src/components/WorkspaceCreationForm/WorkspaceCreationForm.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useWorkspacesContext } from "../../contexts/WorkspacesContext";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
+
+const WorkspaceCreationForm = () => {
+  const navigate = useNavigate();
+  const { createWorkspace, workSpaces } = useWorkspacesContext();
+  const [workspaceName, setWorkspaceName] = useState("");
+  const [channelName, setChannelName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newWorkspace = createWorkspace(workspaceName, channelName);
+    navigate(`/workspace/${newWorkspace.id}/${newWorkspace.channels[0].id}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="workspaceName">Nombre del entorno de trabajo</label>
+      <input
+        type="text"
+        name="workspaceName"
+        required
+        placeholder="Espacio 1"
+        value={workspaceName}
+        onChange={(e) => setWorkspaceName(e.target.value)}
+      />
+      <label htmlFor="channelName">Nombre del canal</label>
+      <input
+        type="text"
+        name="channelName"
+        required
+        placeholder="Saludos"
+        value={channelName}
+        onChange={(e) => setChannelName(e.target.value)}
+      />
+      <div className="create-section">
+        <button
+          onClick={() => navigate("/")}
+          className="create-link"
+          type="button"
+        >
+          Cancelar
+        </button>
+        <button className="create-link" type="submit">
+          Crear Entorno
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default WorkspaceCreationForm;
--- src/pages/NewWorkspacePage/NewWorkspacePage.jsx
+++ src/pages/NewWorkspacePage/NewWorkspacePage.jsx
@@ -1,58 +1,13 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./styles.css";
-import { useWorkspacesContext } from "../../contexts/WorkspacesContext";
+import WorkspaceCreationForm from "../../components/WorkspaceCreationForm/WorkspaceCreationForm";
 
 const NewWorkspacePage = () => {
-  const navigate = useNavigate();
-  const { createWorkspace, workSpaces } = useWorkspacesContext();
-  const [workspaceName, setWorkspaceName] = useState("");
-  const [channelName, setChannelName] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newWorkspace = createWorkspace(workspaceName, channelName);
-    navigate(`/workspace/${newWorkspace.id}/${newWorkspace.channels[0].id}`);
-  };
-
   return (
     <div className="new-workspace-container">
       <h1>Crea un entorno de trabajo</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="workspaceName">Nombre del entorno de trabajo</label>
-        <input
-          type="text"
-          name="workspaceName"
-          required
-          placeholder="Espacio 1"
-          value={workspaceName}
-          onChange={(e) => setWorkspaceName(e.target.value)}
-        />
-        <label htmlFor="channelName">Nombre del canal</label>
-        <input
-          type="text"
-          name="channelName"
-          required
-          placeholder="Saludos"
-          value={channelName}
-          onChange={(e) => setChannelName(e.target.value)}
-        />
-        <div className="create-section">
-          <button
-            onClick={() => navigate("/")}
-            className="create-link"
-            type="button"
-          >
-            Cancelar
-          </button>
-          <button className="create-link" type="submit">
-            Crear Entorno
-          </button>
-        </div>
-      </form>
+      <WorkspaceCreationForm />
     </div>
   );
 };
 
 export default NewWorkspacePage;
-
