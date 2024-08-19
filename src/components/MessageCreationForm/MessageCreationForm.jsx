dbe9afc018f4d13c706d06c1d6833760ff15fa5a
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-19 19:19:32 -0300

    MessageCreationForm refactor

--- src/components/MessageCreationForm/MessageCreationForm.jsx
+++ src/components/MessageCreationForm/MessageCreationForm.jsx
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useWorkspacesContext } from "../../contexts/WorkspacesContext";
+import SearchGIF from "../SearchGIF/SearchGIF";
 import "./styles.css";
-
 const MessageCreationForm = () => {
   const { createMessage } = useWorkspacesContext();
   const { id_workspace, id_channel } = useParams();
   const [newMessage, setNewMessage] = useState("");
+  const [toggleSearchGIF, setToggleSearchGIF] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createMessage(newMessage, id_workspace, id_channel);
+    handleCreateMessage(newMessage)
     setNewMessage("");
   };
 
+  const handleCreateMessage = (message) => {
+    createMessage(message, id_workspace, id_channel);
+  }
+
   return (
-    <form className="message-form" onSubmit={handleSubmit}>
-      <input
-        className="message-text"
-        type="text"
-        placeholder="Escribe el mensaje"
-        required
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-      />
-      <button className="send-button" type="submit">
-        Enviar
-      </button>
+    <form
+      className={`message-form ${toggleSearchGIF && "expanded"}`}
+      onSubmit={handleSubmit}
+    >
+      {toggleSearchGIF ? (
+        <SearchGIF
+          toggleSearchGIFVisibility={() => setToggleSearchGIF(!toggleSearchGIF)}
+          handleCreateMessage= {handleCreateMessage}
+        />
+      ) : (
+        <>
+          <input
+            className="message-text"
+            type="text"
+            placeholder="Escribe el mensaje"
+            required
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button className="send-button" type="submit">
+            Enviar
+          </button>
+          <button
+            className="gif-button"
+            type="button"
+            onClick={() => setToggleSearchGIF(!toggleSearchGIF)}
+          >
+            gif
+          </button>
+        </>
+      )}
     </form>
   );
 };
