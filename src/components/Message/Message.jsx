5df42f3c1781b6d7595855519de845428308bf52
Gaston_Riecan <g.e.riecan@gmail.com>
2024-08-19 17:59:54 -0300

    Auto scroll to show last message, gif rendered inside messages.

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,19 @@
 import { users } from "../../data/data.js";
 import "./styles.css";
+import { useRef, useEffect } from "react";
 
 const Message = ({ message }) => {
   const user = users.find((user) => user.id == message.userID);
+  const messageRef = useRef(null);
+
+  useEffect(() => {
+    if (messageRef.current) {
+      messageRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [message]);
 
   return (
-    <section className="full-message">
+    <section className="full-message" ref={messageRef}>
       <div className="message-container">
         <div className="user-info">
           <img src={`/images/profile-pictures/${user.profilePicture}`} />
@@ -13,7 +21,11 @@ const Message = ({ message }) => {
         </div>
         <span>{message.timeStamp}</span>
       </div>
-      <p>{message.content}</p>
+      {message.content.includes(".gif") ? (
+        <img className="gif" src={message.content}></img>
+      ) : (
+        <p>{message.content}</p>
+      )}
     </section>
   );
 };
