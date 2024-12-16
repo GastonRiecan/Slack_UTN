4fcc9a67be13bce2e9aa0665169dc6195045c689
Gaston_Riecan <g.e.riecan@gmail.com>
2024-12-16 20:57:26 -0300

    register modal added

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,8 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./styles.css";
 import useCustomForm from "../../../Hooks/useCustomForm.jsx";
-import {
-  getUnnauthenticatedHeaders,
-  POST,
-} from "../../../fetching/http.fetching.js";
+import { getUnnauthenticatedHeaders, POST } from "../../../fetching/http.fetching.js";
+import { useState } from "react";
 
 const RegisterForm = () => {
   const form_fields = {
@@ -13,12 +11,11 @@ const RegisterForm = () => {
     password: "",
   };
 
-  const { form_values_state, handleChangeInputValue } =
-    useCustomForm(form_fields);
+  const { form_values_state, handleChangeInputValue } = useCustomForm(form_fields);
   const navigate = useNavigate();
   const backendUrl = import.meta.env.VITE_API_URL;
 
-  console.log("BACKEND URL", backendUrl);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleSubmitRegisterForm = async (e) => {
     e.preventDefault();
@@ -26,21 +23,27 @@ const RegisterForm = () => {
       const body = await POST(`${backendUrl}/api/auth/register`, {
         headers: getUnnauthenticatedHeaders(),
         "Access-Control-Allow-Origin": "*",
-      /*  mode: "no-cors",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE", */
-        body: JSON.stringify(form_values_state)
+        body: JSON.stringify(form_values_state),
       });
 
       if (body.ok) {
         console.log("Registro Exitoso, anda a revisar tu casilla de mail!!");
-        navigate("/login");
+        
+        setModalVisible(true);
+        
+        setTimeout(() => {
+          navigate("/login");
+        }, 5000);
       } else {
         console.error("Error en el registro:", body.message);
       }
-      console.log(body);
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
   };
 
   return (
@@ -79,6 +82,16 @@ const RegisterForm = () => {
       <span>
         Si ya tienes cuenta puedes ir a <Link to="/login">Login</Link>
       </span>
+
+      {modalVisible && (
+        <div className="modal-container">
+          <div className="modal-content">
+            <h2>¡Tu registro fue exitoso!</h2>
+            <p>Anda a chequear tu mail que tenes que verificarlo antes de loguearte!!</p>
+            <button className="success-button" onClick={closeModal}>Cerrar</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
