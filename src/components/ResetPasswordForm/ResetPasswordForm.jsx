db624d816a7e91519d15570fdbb59d5985d62740
Gaston_Riecan <g.e.riecan@gmail.com>
2024-12-19 08:24:36 -0300

    reset

--- src/components/ResetPasswordForm/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm/ResetPasswordForm.jsx
@@ -1,50 +1,56 @@
 import { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import extractFormData from "../../../utils/extractFormData.js";
-import { PUT, getUnnauthenticatedHeaders } from "../../../fetching/http.fetching.js";
+import {
+  PUT,
+  getUnnauthenticatedHeaders,
+} from "../../../fetching/http.fetching.js";
 import "./styles.css";
 
 const ResetPasswordForm = () => {
   const { reset_token } = useParams();
   const backendUrl = import.meta.env.VITE_API_URL;
-  const [error, setError] = useState(null);  
-  const [success, setSuccess] = useState(null);  
-  const [isSubmitting, setIsSubmitting] = useState(false);  
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmitResetForm = (e) => {
-    e.preventDefault();
-    const form_HTML = e.target;
-    const form_Values = new FormData(form_HTML);
-    const form_fields = {
-      password: "",
-    };
-    const form_values_object = extractFormData(form_fields, form_Values);
+    try {
+      e.preventDefault();
+      const form_HTML = e.target;
+      const form_Values = new FormData(form_HTML);
+      const form_fields = {
+        password: "",
+      };
+      const form_values_object = extractFormData(form_fields, form_Values);
 
-    setIsSubmitting(true); 
+      setIsSubmitting(true);
 
-    PUT(`${backendUrl}/api/auth/reset-password/${reset_token}`, {
-      headers: getUnnauthenticatedHeaders(),  
-      body: JSON.stringify(form_values_object),  
-    })
-      .then((response) => {
-        console.log({ response });
-
-        if (response.ok) {
-          setSuccess('Contraseña restablecida correctamente');
-          setError(null);  
-        } else {
-          setError(response.detail);
-          setSuccess(null);  
+      const response = PUT(
+        `${backendUrl}/api/auth/reset-password/${reset_token}`,
+        {
+          headers: getUnnauthenticatedHeaders(),
+          body: JSON.stringify(form_values_object),
         }
-      })
-      .catch((error) => {
-        console.error(error);
-        setError("Ocurrió un error inesperado. Intenta nuevamente.");
-        setSuccess(null);  
-      })
-      .finally(() => setIsSubmitting(false));  
+      );
+      if (response.ok) {
+        setSuccess(
+          "¡La password se actualizo correctamente, ya te podes ir a loguear!"
+        );
+        setError(null);
+        setIsSubmitting(false);
+      } else {
+        setError(
+          "Hubo un problema al actualizar la password. Intenta nuevamente."
+        );
+        setSuccess(null);
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+      setError("Ocurrió un error inesperado. Por favor, intenta nuevamente.");
+      setSuccess(null);
+    }
   };
-
   return (
     <div>
       {error && (
@@ -72,7 +78,13 @@ const ResetPasswordForm = () => {
         <p>Completa el formulario con la nueva contraseña para restablecer:</p>
         <div>
           <label htmlFor="password">Ingrese su nueva contraseña:</label>
-          <input name="password" id="password" placeholder="***" type="password" required />
+          <input
+            name="password"
+            id="password"
+            placeholder="***"
+            type="password"
+            required
+          />
         </div>
         <button type="submit" disabled={isSubmitting}>
           {isSubmitting ? "Restableciendo..." : "Restablecer contraseña"}
